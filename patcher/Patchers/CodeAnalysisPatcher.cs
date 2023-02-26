2ac42bdc0afe2335e312be7ad440e5fe6a30563f
lotuspar <91263142+lotuspar@users.noreply.github.com>
2023-02-27 06:33:32 +0800

    add verbose output to `CodeAnalysisPatcher`

--- patcher/Patchers/CodeAnalysisPatcher.cs
+++ patcher/Patchers/CodeAnalysisPatcher.cs
@@ -7,6 +7,7 @@ public class CodeAnalysisPatcher : IPatcher
 {
 	public bool Patch()
 	{
+		Static.Info( $"Reading assembly @ {Path} ..." );
 		var assembly = AssemblyDefinition.ReadAssembly( Path,
 			new ReaderParameters { ReadWrite = true, ReadingMode = ReadingMode.Immediate, InMemory = true } );
 		if ( assembly == null )
@@ -16,34 +17,40 @@ public class CodeAnalysisPatcher : IPatcher
 		}
 
 		// Create our static constructor
+		Static.Info( "Generating static constructor definition" );
 		var cctor = new MethodDefinition( ".cctor",
 			MethodAttributes.Static | MethodAttributes.HideBySig | MethodAttributes.SpecialName |
 			MethodAttributes.RTSpecialName, assembly.MainModule.TypeSystem.Void );
 
 		// Get the type we want to patch (SyntaxNode)
+		Static.Info( "Looking for Microsoft.CodeAnalysis.SyntaxNode..." );
 		var type = assembly.MainModule.GetType( "Microsoft.CodeAnalysis.SyntaxNode" );
 		if ( type == null )
 		{
-			Static.Err( $"Couldn't find type Microsoft.CodeAnalysis.SyntaxNode" );
+			Static.Err( "Couldn't find type Microsoft.CodeAnalysis.SyntaxNode" );
 			return false;
 		}
 
 		// Add our static constructor to the type
+		Static.Info( "Adding static constructor to SyntaxNode..." );
 		type.Methods.Add( cctor );
 
 		// Make sure the type doesn't have BeforeFieldInit set
+		Static.Info( "Making sure BeforeFieldInit isn't set" );
 		type.Attributes &= ~TypeAttributes.BeforeFieldInit;
 
 		// Create IL for the constructor
+		Static.Info( "Creating IL for the static constructor" );
 		var processor = cctor.Body.GetILProcessor();
 		var patch = IPatcher.CreateLoadInstructions( ref processor, assembly.MainModule );
 
+		Static.Info( "Appending generated IL..." );
 		foreach ( var instruction in patch ) processor.Append( instruction );
 		processor.Append( processor.Create( OpCodes.Ret ) );
 
 		// Write new assembly
+		Static.Info( $"Writing assembly @ {OutputPath} ..." );
 		assembly.Write( OutputPath );
-
 		return true;
 	}
 
